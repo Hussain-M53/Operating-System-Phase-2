a26c1a0b3525367b1579b304bf59aef6f9193f33
Hussain-53 <hash.hussain53@gmail.com>
2022-11-12 22:13:05 +0500

    modified:   main/OS/OS.java 	modified:   main/PCB/PCB.java 	modified:   main/PROCESS/Process.java

--- main/OS/OS.java
+++ main/OS/OS.java
@@ -26,12 +26,21 @@ public class OS {
     }
 
     public static void execute() {
+        PCB pcb ;
         if (!HIGH_PRIORITY_QUEUE.isEmpty()) {
-            PCB pcb = HIGH_PRIORITY_QUEUE.remove();
+            pcb = HIGH_PRIORITY_QUEUE.remove();
             RUNNING_QUEUE.add(pcb);
             running_process.get_from_pcb(pcb);
         } else {
-
+            pcb = LOW_PRIORITY_QUEUE.remove();
+            RUNNING_QUEUE.add(pcb);
+            running_process.get_from_pcb(pcb);
+        }
+        boolean switch_context = !running_process.execute_instr();
+        if(switch_context){
+            pcb = running_process.transfer_to_pcb();
+            byte priority = pcb.get_PROCESS_PRIORITY();
+            
         }
     }
 
